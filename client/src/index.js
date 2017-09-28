ccfe2e6ce980d8a507c8241e721e718d810af93e
Evan Harris <echarris@smcm.edu>
2017-09-28 19:15:43 -0400

    Removes store troubleshooting console.logs

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
-import { fetchTasks, fetchTasksRequest } from './redux/actions/actions';
-
-store.dispatch(fetchTasks());
-store.dispatch(fetchTasksRequest());
-console.log('Text', store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
