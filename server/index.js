d21aa26fbdbe4a6e5b4e027ab17fae9dc5142be1
Evan Harris <echarris@smcm.edu>
2017-09-29 18:03:07 -0400

    Implements google oauth feature with testing

--- server/index.js
+++ server/index.js
@@ -1,55 +1,147 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
+require("dotenv").config();
 
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const {DATABASE, CLIENT_ID, CLIENT_SECRET} = require('./config/secret');
+const path = require("path");
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const BearerStrategy = require("passport-http-bearer").Strategy;
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const { DATABASE, CLIENT_ID, CLIENT_SECRET } = require("./config/secret");
+const { User } = require("./models/user");
 
 const app = express();
-app.use(morgan('common'));
+app.use(morgan("common"));
 app.use(bodyParser.json());
 
+// Mongoose default promise library is deprecated - so we use global promises
 mongoose.Promise = global.Promise;
-
-mongoose.connect(DATABASE, {
-  useMongoClient: true,
-  /* other options */
-});
 
 let secret = {
   CLIENT_ID,
   CLIENT_SECRET
 };
 // Serve the built client
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Allow CORS
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // Unhandled requests which aren't for the API should serve index.html so
 // client-side routing using browserHistory can function
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
-  const index = path.resolve(__dirname, '../client/build', 'index.html');
+  const index = path.resolve(__dirname, "../client/build", "index.html");
   res.sendFile(index);
 });
 
-app.get('/api/tasks', (req,res) => {
-  res.send(200).send('SOme text and stuff');
+// Passport Strategies
+app.use(passport.initialize());
+passport.use(
+  new GoogleStrategy({
+    clientID: secret.CLIENT_ID,
+    clientSecret: secret.CLIENT_SECRET,
+    callbackURL: '/api/auth/google/callback'
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    User
+      .findOne({ googleId: profile.id })
+      .then(user => {
+        if (user) {
+          user.accessToken = accessToken;
+          return user.save();
+        } else {
+          User
+            .create({
+              displayName: profile.displayName,
+              googleId: profile.id,
+              accessToken
+            })
+            .then(console.log('this worked!'))
+            .catch(err => {
+              console.error(err);
+            });
+        }
+      });
+    const user = {
+      googleId: profile.id,
+      accessToken: accessToken
+    };
+    return cb(null, user);
+  }
+  ));
+
+passport.use(
+  new BearerStrategy(
+    (token, done) => {
+      User
+        .findOne({accessToken: token})
+        .then(user => {
+          if (!user) {
+            return done(null, false);
+          } else {
+            return done(null, user);}
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  )
+);
+
+app.get('/api/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/api/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/',
+    session: false
+  }),
+  (req, res) => {
+    res.cookie('accessToken', req.user.accessToken, { expires: 0 });
+    res.redirect('/');
+  }
+);
+
+app.get('/api/auth/logout', (req, res) => {
+  req.logout();
+  res.clearCookie('accessToken');
+  res.redirect('/');
 });
+
+// Endpoints that do not have to do with authentication:
+  // usermission
+  // userdata
+  // user?
+
+// app.get(/user/data)
+// app.get("/api/tasks", (req, res) => {
+//   res.send(200).send("SOme text and stuff");
+// });
+
 let server;
-function runServer(port=3001) {
+function runServer(port = 3001) {
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      resolve();
-    }).on('error', reject);
+    mongoose.connect(DATABASE, {useMongoClient: true}, err => {
+      console.log('Starting server')
+      if (err) {
+        return reject(err);
+      }
+    });
+    server = app
+      .listen(port, () => {
+        resolve();
+      })
+      .on("error", reject);
   });
 }
 
@@ -66,11 +158,12 @@ function closeServer() {
 
 if (require.main === module) {
   runServer().catch(err => {
-    console.error('Problem starting server: ', err);
+    console.error("Problem starting server: ", err);
   });
 }
 
 module.exports = {
-  app, runServer, closeServer
+  app,
+  runServer,
+  closeServer
 };
-
--- server/test/test-integration.js
+++ server/test/test-integration.js
@@ -3,17 +3,96 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 
-const {app, runServer, closeServer} = require('../index');
-const {DATABASE, CLIENT_ID, CLIENT_SECRET} = require('../config/secret');
+const { app, runServer, closeServer } = require('../index');
+const { DATABASE, CLIENT_ID, CLIENT_SECRET } = require('../config/secret');
+const { User } = require('../models/user');
 
 const should = chai.should();
-// Fake test only to check that pathing in package.json is okay for server side testing
-describe('Test', function () {
-  it('should succeed', function () {
-    true.should.be.true;
+chai.use(chaiHttp);
+
+const testUser = {
+  displayName: 'Evan Harris',
+  googleId: '113991032114835833364',
+  accessToken:
+    'ya29.GlvVBK1WhImxQgRZGD9yanjRErwHcEmY6aQy2IFvzLli7WHPGW4Fv4iy2y1DwagsW9Qb8FEOJm_CfclLUAbRzocyina4RvRLrx5_92c-6A7A2_pXZZyg7ItY2e8Z'
+};
+
+const seedUserData = user => {
+  return User.create(user);
+};
+const tearDownDatabase = () => {
+  return new Promise((resolve, reject) => {
+    mongoose.connection
+      .dropDatabase()
+      .then(result => {
+        return resolve(result);
+      })
+      .catch(err => {
+        return reject(err);
+      });
   });
-});
+};
 
 describe('Life coach', () => {
+  // Testing life cycle methods
+  before(() => runServer());
+
+  after(() => closeServer());
+
+  beforeEach(() => {
+    // Use Promise all if we need to seed more data
+    // return Promise.all([seedUserData(), seedOtherData()]);
+    return seedUserData(testUser);
+  });
+
+  afterEach(() => {
+    // delete whatever seeded data we do not want to persist to the next test
+    return tearDownDatabase();
+  });
+
+  describe('Google authentication', () => {
+
+    // Example User Query: 
+    // User.findOne({ googleId: testUser.googleId }).then(_user => {
+    //   console.log('User: ', _user);
+    // });
+    it('should redirect to google authentication', () => {
+      chai.request(app)
+        .get('/api/auth/google').redirects(0)
+        .set('Authorization', `Bearer ${testUser.accessToken}`)
+        .end((err, res) => {
+          // Could maybe refactor this for loop in a fashiong similar to the logout test
+          // where we look for the prescense of the googleUrl terminating in the '?'
+          let googleUrl =''; 
+          let currentChar;
+          for (let i = 0; i < res.headers['location'].length; i++) {
+            currentChar = res.headers['location'][i];
+            if (currentChar !== '?') {
+              googleUrl += currentChar;
+            } else if (currentChar === '?') {
+              break;
+            }
+          }
+          res.should.have.status(302);
+          googleUrl.should.equal('https://accounts.google.com/o/oauth2/v2/auth');
+        });
+    });
+
+    it('should logout the user', () => {
+      chai.request(app)
+        .get('/api/auth/logout').redirects(0)
+        .end((err, res) => {
+          res.should.have.status(302);
+          res.headers['location'].should.be.equal('/');
+          res.headers['set-cookie'][0].should.contain('accessToken=;');
+        });
+    });
+  });
+
+  describe('GET requests', () => {});
+
+  describe('POST requests', () => {});
+
+  describe('PUT requests', () => {});
 
 });
