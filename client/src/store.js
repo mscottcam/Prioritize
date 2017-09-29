9ed1c69bbdd5a30310e09bc81c399133ddbbdf24
Lewi Gilamichael <dr.lewi@gmail.com>
2017-09-29 11:20:41 -0400

    updated store

--- client/src/store.js
+++ client/src/store.js
@@ -2,5 +2,5 @@ import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './redux/reducers/';
 import thunk from 'redux-thunk';
 
-const store = createStore(taskReducer, window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION(), applyMiddleware(thunk))
+const store = createStore(rootReducer, window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION(), applyMiddleware(thunk))
 export default store;
