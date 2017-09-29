d7add8c531c0e4bbd229056ac94aa7d335fb5422
Lewi Gilamichael <dr.lewi@gmail.com>
2017-09-29 17:35:58 -0400

    added tests and reducers for actions

--- client/src/redux/reducers/taskReducer.test.js
+++ client/src/redux/reducers/taskReducer.test.js
@@ -3,33 +3,67 @@ import * as actions from '../actions'
 import taskReducer from './taskReducer'
 import missionReducer from './missionReducer'
 
+const initialState = {
+  currentUser: null, 
+  userId: null,
+  currentMission: null,
+  roles:[],
+  goals: [],
+  projects: [],
+  tasks: []
+};
+const userData = {
+  currentUser: 'lewi',
+  userId: 61518,
+  roles:[
+    {role:'uncle'}
+  ],
+  goals: [],
+  projects: [],
+  tasks: [
+    {
+      taskId: 5678,
+      task: 'do homework on monday'}
+  ]
+};
+
 describe('task reducer', () => {
-  xit('should set state to initial state when no arguments', () => {
+  it('should set state to initial state when no arguments', () => {
     const newState = taskReducer(undefined, {type: 'test'});
     expect(newState.currentUser).toEqual(null);
     expect(newState.tasks).toEqual([]);
   });
 
-  xit('should return current state on unknown action', () => {
-    let state = {};
-    const newState = taskReducer(state, undefined);
-    expect(newState).toEqual({});
+  it('should return current state on unknown action', () => {
+    let state = 'whot he don';
+    const newState = taskReducer(state, 'ehsy yfljg');
+    expect(newState).toEqual(state);
   })
-    it('should handle FETCH_USER_DATA_SUCCESS action', () => {
-      let state = {};
-    const newState = taskReducer(state, {type: 'FETCH_USER_DATA_SUCCESS'});
-    expect(newState.currentUser).toEqual(true);
-    expect(newState.tasks).toEqual([]);
+  it('should handle FETCH_USER_DATA_SUCCESS action', () => {
+    let state = initialState;
+    const newState = taskReducer(state, actions.fetchUserDataSuccess(userData));
+    expect(newState.currentUser).toEqual(userData.currentUser);
+    expect(newState.roles).toEqual(userData.roles);
+    expect(newState.goals).toEqual(userData.goals);
+    expect(newState.projects).toEqual(userData.projects);
+    expect(newState.tasks).toEqual(userData.tasks);
   });
-  // it('should handle POST_TASK_SUCCESS', () => {
-  //   const newState = taskReducer(undefined, {type: 'POST_TASK_SUCCESS'});
-  //   expect(newState.currentUser).toEqual(true);
-  //   expect(newState.tasks).toInclude(action.task);
-  // });
-  // it('should handle UPDATE_TASK_SUCCESS', () => {
-  //   const newState = taskReducer(undefined, {type: 'UPDATE_TASK_SUCCESS'});
-  //   expect(newState.currentUser).toEqual(true);
-  //   expect(newState.tasks).toEqual([]);
-  // });
+  it('should handle POST_TASK_SUCCESS', () => {
+    let state = userData;
+    let newTask = {
+      taskId: 3456,
+      task:'what the hell is this'};
+    const newState = taskReducer(state, actions.postTaskSuccess(newTask));
+    expect(newState.tasks).toContainEqual(newTask);
+  });
+  it('should handle UPDATE_TASK_SUCCESS', () => {
+    let state = userData;
+    let updateTask = {
+      taskId: 5678,
+      task: 'postpone homework'
+    }
+    const newState = taskReducer(state, actions.updateTaskSuccess(updateTask));
+    expect(newState.tasks.find(item => item.taskId ===updateTask.taskId)).toEqual(updateTask);
+  });
 
 });
