05bcb462440be718eaf8a101ccae23ffe6370c9b
Lewi Gilamichael <dr.lewi@gmail.com>
2017-10-02 17:32:39 -0400

    added logIn, header and react-redux

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import LogIn from '../logIn'
 // import UserInfo from '.userInfo';
 // header should have the name of our App
 // Header should have the userInfo component which has the username and logout buttons
 
-export default function Header() {
-    return (
-        <div className="card">
-            <h1>Prioritize</h1>
-            {/* <UserInfo /> */}
-        </div>
-    );
-};
+export default class Header extends React.Component {
+  render(){
+  return (
+     <div className="card">
+					<h1>Prioritize</h1>
+						<LogIn /> 
+			</div>
+		);
+	};
+}
