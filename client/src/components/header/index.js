61e95ef4a70b637b3283482216244f1e2d39349b
Lewi Gilamichael <dr.lewi@gmail.com>
2017-10-02 13:03:25 -0400

    added userInfo, edited Header and added more tasks.test

---
+++ client/src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserInfo from '../userInfo';
+// header should have the name of our App
+// Header should have the userInfo component which has the username and logout buttons
+
+export default function Header() {
+    return (
+        <div className="card">
+            <h1>Prioritize</h1>
+            <UserInfo />
+        </div>
+    );
+};
---
+++ client/src/components/userInfo/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+// should have the userName and a logOut button
