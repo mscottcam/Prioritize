c2c5e60c4fa41ca57005b8b940cad9b16e31d01e
Evan Harris <echarris@smcm.edu>
2017-10-02 21:25:10 -0400

    Reverts changes to dev dependencies, travis node_env, and config files

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,14 +1,8 @@
 'use strict';
-require('dotenv').config();
 
-exports.CLIENT_ID = process.env.CLIENT_ID,
-exports.CLIENT_SECRET = process.env.CLIENT_SECRET,
-exports.DATABASE = process.env.DATABASE,
-exports.TEST_DATABASE = process.env.TEST_DATABASE;
-
-// module.exports = {
-//   CLIENT_ID: process.env.CLIENT_ID,
-//   CLIENT_SECRET: process.env.CLIENT_SECRET,
-//   DATABASE: process.env.DATABASE,
-//   TEST_DATABASE: process.env.TEST_DATABASE
-// };
+module.exports = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  DATABASE: process.env.DATABASE,
+  TEST_DATABASE: process.env.TEST_DATABASE
+};
