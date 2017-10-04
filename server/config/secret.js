e891fc161d30051e5f6a0ee2a057523c1777c34d
Evan Harris <echarris@smcm.edu>
2017-10-04 16:02:36 -0400

    Adds temporary credentials to troubleshoot travis build

---
+++ server/config/secret.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  DATABASE: process.env.DATABASE,
+  TEST_DATABASE: process.env.TEST_DATABASE
+};
