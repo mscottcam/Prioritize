e9c228f2f2deded5dcca481cec62c691b95ad02b
Lewi Gilamichael <dr.lewi@gmail.com>
2017-10-10 11:13:36 -0400

    Initial test passing

--- server/config.js
+++ server/config.js
@@ -1,16 +1,7 @@
 'use strict';
-<<<<<<< HEAD
 require('dotenv').config();
 
 exports.CLIENT_ID = process.env.CLIENT_ID || global.CLIENT_ID;
 exports.CLIENT_SECRET = process.env.CLIENT_SECRET || global.CLIENT_SECRET;
 exports.DATABASE = process.env.DATABASE || global.DATABASE;
 exports.TEST_DATABASE = process.env.TEST_DATABASE || global.TEST_DATABASE;
-=======
-
-require('dotenv').config();
-exports.CLIENT_ID = process.env.CLIENT_ID || global.CLIENT_ID || '181298905302-j478c09tjhinh35oh0gigkdk410t3trj.apps.googleusercontent.com';
-exports.CLIENT_SECRET = process.env.CLIENT_SECRET || global.CLIENT_SECRET || '8aSA2Q2DUZ4k4zVNR95u0htU';
-exports.DATABASE = process.env.DATABASE || global.DATABASE || 'mongodb://dev:password@ds155414.mlab.com:55414/life-coach';
-exports.TEST_DATABASE = process.env.TEST_DATABASE || global.TEST_DATABASE || 'mongodb://dev:password@ds157444.mlab.com:57444/prioritize-test';
->>>>>>> 0574635e7ed7584446b3bdef218fd93be1784a6b
